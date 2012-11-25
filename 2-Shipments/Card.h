3fa584d67642c20999f7c906ea9495646c7a59a2
Moshe Berman <moshberm@gmail.com>
2012-11-25 16:09:42 -0500

    Added a Card class to the project.

---
+++ 2-Shipments/Card.h
@@ -0,0 +1,21 @@
+//
+//  Card.h
+//  2-Shipments
+//
+//  Created by Moshe Berman on 11/25/12.
+//  Copyright (c) 2012 Moshe Berman. All rights reserved.
+//
+
+#ifndef __Shipments_Card_h
+#define __Shipments_Card_h
+
+class Card {
+public:
+    char cardType;
+    string city;
+    int amount1 = 0;
+    int amount2 = 0;
+    int amount3 = 0;
+};
+
+#endif
