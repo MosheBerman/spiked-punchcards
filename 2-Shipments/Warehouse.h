e4d10168bfef54052b8f1bb3d2fcce9673109f8c
Moshe Berman <moshberm@gmail.com>
2012-11-25 17:34:15 -0500

    Added a description to the Warehouse class.

--- 2-Shipments/Warehouse.h
+++ 2-Shipments/Warehouse.h
@@ -9,12 +9,27 @@
 #ifndef __Shipments_Warehouse_h
 #define __Shipments_Warehouse_h
 
+#include <sstream>
+
 using namespace std;
 
 class Warehouse {
 public:
     string name;
     double quantities[3];
+    
+    //
+    //  Returns a string representing the data in the Warehouse
+    //
+    
+    string description(){
+        
+        ostringstream stream;
+
+        stream << name << '\t' << quantities[0] << '\t' << quantities[1] << '\t' << quantities[2] << '\n';
+        
+        return stream.str();
+    }
 };
 
 #endif
